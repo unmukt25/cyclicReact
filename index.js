05c588fc664da5ad43ce2164d2f428b9116f632e
unmukt25 <unmuktjain.rera@gmail.com>
2023-12-23 20:40:50 +0530

    router updated

--- index.js
+++ index.js
@@ -7,8 +7,14 @@ const port=process.env.PORT || 3000;
 const app=express();
 app.use(express.static(path.join(__dirname,'./build')));
 
+
+
 app.get('/work',(req,res)=>{
     res.send('server working');
+});
+
+app.get('*',(req,res)=>{
+    res.sendFile(path.join(__dirname,'./build/index.html'));
 });
 
 app.listen(port,()=>{
